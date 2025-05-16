86d1bf6da6147ddc274ce6a8282d352ea904eef3
TrashTrain <96978670+TrashTrain@users.noreply.github.com>
2025-05-16 11:32:55 +0900

    update : setting

--- Assets/Scripts/Tiger/Test/TestLevelChanger.cs
+++ Assets/Scripts/Tiger/Test/TestLevelChanger.cs
@@ -7,10 +7,21 @@ using UnityEngine.UI;
 public class TestLevelChanger : MonoBehaviour
 {
     public TMP_InputField changeText;
+    private PlayerTest player = GameMgr.Instance.PlayerInit();
     public void OnClickLevelChanger()
     {
-        PlayerTest player = GameMgr.Instance.PlayerInit();
         player.SetBoundaryLevel(int.Parse(changeText.text));
         Debug.Log(player.GetBoundaryLevel());
+    }
+
+    public void OnClickLevelUpBtn()
+    {
+        player.SetUpBoundaryLevel();
+        changeText.text = player.GetBoundaryLevel().ToString();
+    }
+    public void OnClickLevelDownBtn()
+    {
+        player.SetDownBoundaryLevel();
+        changeText.text = player.GetBoundaryLevel().ToString();
     }
 }
