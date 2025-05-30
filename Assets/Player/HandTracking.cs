b926c4afa210915568680d950dbc16fa88359f43
TrashTrain <96978670+TrashTrain@users.noreply.github.com>
2025-05-30 09:42:58 +0900

    fix : scripts

---
+++ Assets/Player/HandTracking.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.InputSystem;
+using UnityEngine;
+
+public class HandTracking : MonoBehaviour
+{
+    public InputActionProperty pinchAnimaionAction;
+    public InputActionProperty gripAnimtionAction;
+
+    public Animator HandAnimator;
+
+    void Update()
+    {
+        float triggerValue = pinchAnimaionAction.action.ReadValue<float>();
+        HandAnimator.SetFloat("Trigger", triggerValue);
+        float gripValue = gripAnimtionAction.action.ReadValue<float>();
+        HandAnimator.SetFloat("Grip", gripValue);
+        
+    }
+}
